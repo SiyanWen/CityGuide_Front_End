4622cdd6c026cf3f0b3a465fd5b52147bb74b56b
JHL625 <jinh.lv@gmail.com>
2024-09-30 10:31:18 +0800

    CityGuide Map initial version 1.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,44 @@
+"use client";
+
 import React from "react";
-import CustomMap from "./CustomMap";
-import {APIProvider} from '@vis.gl/react-google-maps'
+// import { createRoot } from "react-dom/client";
+import {APIProvider, Map,} from "@vis.gl/react-google-maps";
 
+const API_KEY= process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 
-
-function App(){
+const App=()=>{
+  // const position ={ lat:47.608013,lng:-122.335167,};
   return(
-    <div className="app">
-      {/* <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY}>
-        <CustomMap/>
-      </APIProvider> */}
-      <CustomMap/>
-    </div>
+    <APIProvider 
+    solutionChannel="GMP_devsite_samples_v3_rgmbasicmap"
+    apikey={API_KEY}>
+      <div style={{height: "100vh"}}>
+        <Map 
+        defaultZoom={8} 
+        defaultCenter={{ lat:47.608013,lng:-122.335167}} 
+        mapId={process.env.MAP_ID}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}/>
+      </div>;
+    </APIProvider>
   );
+
 }
 
 export default App;
+
+// import React from "react";
+// import CustomMap from "./CustomMap";
+// import {APIProvider} from '@vis.gl/react-google-maps'
+
+
+
+// const App=()=>{
+//   return(
+//     <div className="app">
+//       <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY}>
+//         <CustomMap/>
+//       </APIProvider>
+//     </div>
+//   );
+// }
--- src/components/CustomMap.js
+++ src/components/CustomMap.js
@@ -1,57 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+// import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import {Map, AdvancedMarker} from "@vis.gl/react-google-maps"
 
 const CustomMap = () => {
-  const [map, setMap] = useState(null);
+  const[markerLocation, setMarkerLocation]=useState({lat:47.608013,lng:-122.335167,}); //seatlle
 
-  const containerStyle = {
-    width: "100%",
-    height: "100%",
-  };
+  return(
+      <div className='map-container'>
+          <Map
+          defaultZoom={13}
+          defaultCenter={markerLocation}
+          gestureHandling={"greedy"}
+          disableDefaultUI
+          />
 
-  const center = {
-    lat: 47.608013,
-    lng: -122.335167,
-  };
-
-  useEffect(() => {
-    // Check if the API key is available in the environment variables
-    if (!process.env.REACT_APP_GOOGLE_MAPS_API_KEY) {
-      console.error("Google Maps API key not found.");
-      return;
-    }
-  }, []);
-
-  return (
-    <LoadScript
-      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-      libraries={["places"]}
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={(map) => setMap(map)}
-      >
-        {/* Add your map markers or other components here */}
-      </GoogleMap>
-    </LoadScript>
+      </div>
   );
-};
 
-// const[markerLocation, setMarkerLocation]=useState({lat:47.608013,lng:-122.335167,}); //seatlle
+    // const [map, setMap] = useState(null);
 
-// return(
-//     <div className='map-container'>
-//         <Map
-//         defaultZoom={13}
-//         defaultCenter={markerLocation}
-//         gestureHandling={"greedy"}
-//         disableDefaultUI
-//         >
-//             <AdvancedMarker position={markerLocation}/>
-//         </Map>
-//     </div>
-// );
+  // const containerStyle = {
+  //   width: "100%",
+  //   height: "100%",
+  // };
+
+  // const center = {
+  //   lat: 47.608013,
+  //   lng: -122.335167,
+  // };
+
+  // useEffect(() => {
+  //   // Check if the API key is available in the environment variables
+  //   if (!process.env.REACT_APP_GOOGLE_MAPS_API_KEY) {
+  //     console.error("Google Maps API key not found.");
+  //     return;
+  //   }
+  // }, []);
+
+
+  // return (
+  //   <LoadScript
+  //     googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+  //     libraries={["places"]}
+  //   >
+  //     <GoogleMap
+  //       mapContainerStyle={containerStyle}
+  //       center={center}
+  //       zoom={10}
+  //       onLoad={(map) => setMap(map)}
+  //     >
+  //       {/* Add your map markers or other components here */}
+  //     </GoogleMap>
+  //   </LoadScript>
+  // );
+}
 
 export default CustomMap;
