8f69313ffd363a7a26c16c0814e92f44da2be440
Siyan <swen42023@gmail.com>
2024-10-20 03:36:06 -0400

    fixed sign up jump to sign in instead of search page

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,17 @@
 export const login = (credentials) => {
-  const loginUrl = `/login?email=${credentials.email}&password=${credentials.password}`;
+  const loginUrl = `/login?username=${credentials.email}&password=${credentials.password}`;
   // fetch will return something called a Promise
   return fetch(loginUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    body: JSON.stringify(credentials),
   }).then((response) => {
     if (response.status < 200 || response.status >= 300) {
       throw Error("Fail to log in");
     }
-    return response.json(); // Parse and return the response data as JSON
+    // return response.json(); // Parse and return the response data as JSON
   });
 };
 
@@ -27,7 +28,7 @@ export const signup = (data) => {
     if (response.status < 200 || response.status >= 300) {
       throw Error("Fail to sign up");
     }
-    return response.json();
+    // return response.json();
   });
 };
 
@@ -46,64 +47,63 @@ export const logout = () => {
     method: "POST",
     credentials: "include",
   }).then((response) => {
-    if (response.status !== 204) {
+    if (response.status < 200 || response.status >= 300) {
       throw Error("Fail to log out");
     }
   });
 };
 
-
-export const addToUserSpot = ({spot}) => {
-    const payload = {
-        original_spot_gid: spot.place_id,
-        name: spot.name,
-        description: spot.editorial_summary,
-        address: spot.formatted_address,
-        type:spot.type,
-        rating: spot.rating,
-        rating_count: spot.user_ratings_total,
-        cost: spot.price_level,
-        duration_time:spot.opening_hours,
-        image_url: spot.photo_reference,
-        review: spot.reviews,
-        lat: spot.geometry.location.lat,
-        lng: spot.geometry.location.lng,
-    };
-  
-    return fetch(`/cart/spot`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    }).then((response) => {
-      if (response.status < 200 || response.status >= 300) {
-        throw Error("Fail to add spot to MySelection");
-      }else{return true}
-    });
+export const addToUserSpot = ({ spot }) => {
+  const payload = {
+    original_spot_gid: spot.place_id,
+    name: spot.name,
+    description: spot.editorial_summary,
+    address: spot.formatted_address,
+    type: spot.type,
+    rating: spot.rating,
+    rating_count: spot.user_ratings_total,
+    cost: spot.price_level,
+    duration_time: spot.opening_hours,
+    image_url: spot.photo_reference,
+    review: spot.reviews,
+    lat: spot.geometry.location.lat,
+    lng: spot.geometry.location.lng,
   };
 
+  return fetch(`/cart/spot`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to add spot to MySelection");
+    } else {
+      return true;
+    }
+  });
+};
 
-  export const removeSpotFromMySelection = (spotId) => {
-    return fetch(`/cart/spot/${spotId}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((response) => {
-      if (response.status < 200 || response.status >= 300) {
-        throw Error("Fail to delete");
-      }
-    });
-  };
+export const removeSpotFromMySelection = (spotId) => {
+  return fetch(`/cart/spot/${spotId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to delete");
+    }
+  });
+};
 
+export const getMySelection = () => {
+  return fetch("/cart").then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to get MySelection");
+    }
 
-  export const getMySelection = () => {
-    return fetch("/cart").then((response) => {
-      if (response.status < 200 || response.status >= 300) {
-        throw Error("Fail to get MySelection");
-      }
-  
-      return response.json();
-    });
-  };
+    return response.json();
+  });
+};
